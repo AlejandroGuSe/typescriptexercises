16fd634e4ba10cef00986401af7e1c4ce0369269
Alejandro Gutiérrez Serramo <ti.desarrollo5@tjo.mx>
2022-11-29 18:35:59 -0600

    new changes to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-class Person  {
+abstract class Person  {
     constructor(public id: string, public name: string, public city: string){ }
+
+    getDetails(): string {
+        return `${this.name}, ${this.getSpecificDetails()}`
+    }
+
+    abstract getSpecificDetails(): string;
 }
 
 
@@ -11,11 +17,19 @@ class Employee  extends Person{
     writeDept(){
         console.log(`${this.name} work in ${this.dept}`)
     }
+
+    getSpecificDetails(): string {
+        return `works in ${this.dept}`
+    }
 }
 
 class Customer extends Person {
     constructor(public readonly id: string, public name: string, public city: string, public creditLimit: number){
         super(id, name, city)
+    }
+    
+    getSpecificDetails(): string {
+        return `works in ${this.creditLimit}`
     }
 }
 
@@ -24,6 +38,10 @@ class Supplier extends Person{
     constructor(public readonly id: string, public name: string, public city: string, public companyName: string){
             super(id, name, city)
         }
+        
+    getSpecificDetails() {
+            return `works for ${this.companyName}`;
+    }
 }
 
 let data: Person[] = [new Employee("fvega", "Fidel Vega", "Sales", "Paris"),
@@ -31,13 +49,15 @@ let data: Person[] = [new Employee("fvega", "Fidel Vega", "Sales", "Paris"),
 
 data.push(new Supplier("dpeters", "Dora Peters", "New York", "Acme"))
 
- data.forEach(item => {
-        console.log(`Person: ${item.name}, ${item.city}`);
-    if (item instanceof Employee) {
-        item.writeDept();
-    } else if(item instanceof Customer) {
-        console.log(`Customer ${item.name} has ${item.creditLimit} limit`)
-    } else if(item instanceof Supplier){
-        console.log(`Supplier ${item.name} works for ${item.companyName}`)
-    }
-})
+data.forEach(item => console.log(item.getDetails()))
+
+//  data.forEach(item => {
+//         console.log(`Person: ${item.name}, ${item.city}`);
+//     if (item instanceof Employee) {
+//         item.writeDept();
+//     } else if(item instanceof Customer) {
+//         console.log(`Customer ${item.name} has ${item.creditLimit} limit`)
+//     } else if(item instanceof Supplier){
+//         console.log(`Supplier ${item.name} works for ${item.companyName}`)
+//     }
+// })
